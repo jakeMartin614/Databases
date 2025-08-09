2c44d866149ea94d338641e509c9fe2b1e47b3d7
Sophia Munoz <smunnoz22@gmail.com>
2025-08-09 13:43:27 -0400

    borrowedmovies model adjusted

--- LibraryDatabase/Models/BorrowedMovies.cs
+++ LibraryDatabase/Models/BorrowedMovies.cs
@@ -2,15 +2,13 @@ namespace LibraryDatabase.Models
 {
     public class BorrowedMovie
     {
-        public int borrowedMoviesID { get; set; }
-        public DateTime dueDate { get; set; }
-        public DateTime? returnDate { get; set; }
-        public string MemberName { get; set; }
-        public string Title { get; set; }
-
-        // Foreign Keys
         public int MovieID { get; set; } // FK to Movie
         public int MemberID { get; set; } // FK to Member
-  
+        public int borrowedMoviesID { get; set; }
+        public string MemberName { get; set; }
+        public string Title { get; set; }
+        public DateTime dueDate { get; set; }
+        public DateTime? returnDate { get; set; }
+
     }
 }
